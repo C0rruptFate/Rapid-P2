77530f04c067dcb6c92a5cbfecb102ef42444a95
Zhitao <31802he@163.com>
2017-09-14 14:08:29 -0600

    control 0.1

--- js/game.js
+++ js/game.js
@@ -2,6 +2,8 @@ var input;
 var player;
 var bg;
 var ground;
+var PI = 3.1416;
+var R = 180/PI;
 
 var Game = {
 
@@ -18,11 +20,17 @@ var Game = {
         //set sprites 
         bg = game.add.sprite(game.world.centerX, game.world.centerY, 'bg');
         bg.anchor.setTo(0.5, 0.5);
+
         ground = game.add.sprite(game.world.centerX, game.world.centerY, 'ground');
         ground.anchor.setTo(0.5, 0.5);
+
         player = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
         player.anchor.setTo(0.5, 0.5); 
         player.fuel = 100;
+        player.maxSpeed = 50;
+        player.maxAngVelo = 360;
+        player.minAngVelo = -360;
+        player.acce = 0;
 
         //set physics
         game.physics.enable(player, Phaser.Physics.ARCADE);
@@ -41,17 +49,46 @@ var Game = {
 
         //input
         if(input.left.isDown){
-            player.body.angularVelocity = 30;
+            player.body.angularAcceleration = -360;
+            
         }
         else if(input.right.isDown){
-            player.body.angularVelocity = -30;
+            player.body.angularAcceleration = 360;            
         }
+        else{            
+            // if(player.body.angularVelocity < 100)
+            //     player.body.angularAcceleration = 100;
+            // else if(player.body.angularVelocity > 100)
+            //     player.body.angularAcceleration = -100;
+            // else{
+            //     player.body.angularVelocity = 0;
+            //     player.body.angularAcceleration = 0;
+            //     }
+            player.body.angularAcceleration = 0;
+            player.body.angularVelocity = 0;
+        }
+        if(player.body.angularVelocity < player.minAngVelo)
+            player.body.angularVelocity = player.minAngVelo;
+        if(player.body.angularVelocity > player.maxAngVelo)
+            player.body.angularVelocity = player.maxAngVelo;
+
+
+        //console.log(player.body.speed);
 
         if(input.up.isDown){
-            player.body.velocity.x = 30
+            player.acce = 300
         }
         else if(input.down.isDown){
-            player.body.velocity.x = -30;
+            player.acce = -300;
         }
+
+        if(player.body.speed > player.maxSpeed){
+            player.acce = 0;
+        }
+
+        player.body.acceleration.x = Math.sin(player.body.rotation/R) * player.acce;
+        player.body.acceleration.y = Math.cos(player.body.rotation/R) * player.acce * (-1);
+        console.log(player.body.acceleration.x);
+        console.log(player.body.acceleration.y);
     }
 };
