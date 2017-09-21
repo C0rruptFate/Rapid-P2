968baf8c5d701708968c7e1bbc72d28aadec3055
Zhitao <31802he@163.com>
2017-09-21 10:42:57 -0600

    new control and fuel rules, modify UI, add time function, camera follow

--- js/game.js
+++ js/game.js
@@ -1,10 +1,12 @@
+//game components
 var input;
 var player;
 var bg;
 var ground;
-var PI = 3.1416;
-var R = 180/PI;
+var emitter;
+var timer;
 
+//texts
 var scoreText;
 var timeText;
 var fuelText;
@@ -12,10 +14,14 @@ var altiText;
 var speedXText;
 var speedYText;
 
+
+//game data
 var score = 0;
 var time = 0;
 
-var emitter;
+//constants
+var PI = 3.1416;
+var R = 180/PI;
 
 var Game = {
 
@@ -33,37 +39,49 @@ var Game = {
         game.world.setBounds(0, 0, 1600, 900);
         //set sprites 
         bg = game.add.tileSprite(0, 0, 1600, 900, 'bg');
-        //bg = game.add.sprite(0, 0, 'bg');
         ground = game.add.tileSprite(0, 0, 1600, 900, 'ground');
-        //ground = game.add.sprite(0, 0, 'ground');
+        player = game.add.sprite(200, 200, 'player');
 
-        player = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
+        //init physics
+        game.physics.startSystem(Phaser.Physics.P2JS);
+        //game.physics.p2.setImpactEvents(true);
+
+        game.physics.enable(player, Phaser.Physics.P2JS);
+        //game.physics.enable(ground, Phaser.Physics.P2JS); 
+        player.body.gravity.y = 5;
+        // player.body.clearShapes();
+        // player.body.loadPolygon('physicsData', 'player');
+        // ground.body.clearShapes();
+        // ground.body.loadPolygon('physicsData', 'ground');
+
+        //ground.body.immovable = true;
+
+        //init player
         player.anchor.setTo(0.5, 0.5); 
+        player.body.velocity.x = 10; 
         player.fuel = 1000;
         player.maxSpeed = 30;
         player.maxSpeedX = 30;
         player.maxSpeedY = 30;
-        player.maxAngVelo = 720;
-        player.minAngVelo = -720;
+        player.maxAngVelo = 20;
+        player.minAngVelo = -20;
         player.acce = 30;
-
-        //set physics
-        // game.physics.startSystem(Phaser.Physics.P2JS);
-        // game.physics.p2.setImpactEvents(true);
-
-        // var playerCollisionGroup = game.physics.p2.createCollisionGroup();
-        // var groundCollisionGroup = game.physics.p2.createCollisionGroup();
-
-        // game.physics.p2.updateBoundsCollisionGroup();
-        // ground.physicsBodyType = Phaser.Physics.P2JS;
-
-        game.physics.enable(player, Phaser.Physics.ARCADE);
-        game.physics.enable(ground, Phaser.Physics.ARCADE); 
-        player.body.gravity.y = 10;
-        ground.body.immovable = true;
+        player.lives = 3;
+        player.engineLevel = 0;
 
         //init input
         input = game.input.keyboard.createCursorKeys();
+        game.input.keyboard.addKey(Phaser.Keyboard.UP).onDown.add(function(){
+            if(player.engineLevel < 3)
+                player.engineLevel++;
+            console.log(player.engineLevel);
+        }, this);
+
+        game.input.keyboard.addKey(Phaser.Keyboard.DOWN).onDown.add(function(){
+            if(player.engineLevel > 0)
+                player.engineLevel--;
+            console.log(player.engineLevel);
+        }, this);
 
         //init text
         scoreText = game.add.text(16, 16, 'Score: 0', {fontSize:'24px', fill:'#eeeeee'});
@@ -74,46 +92,49 @@ var Game = {
         fuelText.fixedToCamera = true;
         altiText = game.add.text(150, 16, 'Altitude: 0', {fontSize:'24px', fill:'#eeeeee'});
         altiText.fixedToCamera = true;
-        speedXText = game.add.text(150, 48, 'Horizontal Speed: 0', {fontSize:'24px', fill:'#eeeeee'});
-        speedXText.fixedToCamera = true;
-        speedYText = game.add.text(150, 80, 'Vertical Speed: 0', {fontSize:'24px', fill:'#eeeeee'});
-        speedYText.fixedToCamera = true;
+        speedXText = game.add.text(player.x + 30, player.y - 30, 'SpeedX: 0', {fontSize:'12px', fill:'#eeeeee'});
+        speedYText = game.add.text(player.x + 30, player.y - 15, 'SpeedY: 0', {fontSize:'12px', fill:'#eeeeee'});
 
         //init particle
         emitter = game.add.emitter(player.x, player.y, 100);
         emitter.makeParticles('particle');
-        emitter.minParticleScale = 0.8;
-        emitter.maxParticleScale = 1.2;
+        emitter.minParticleScale = 0.5;
+        emitter.maxParticleScale = 1.5;
         emitter.bounce.y = 0.5;
         emitter.gravity = 0;
-        emitter.start(false, 200, 5, 0, false);     
+        emitter.start(false, 200, 5, 0, false); 
+
+        //init time
+        timer = game.time.create(false);
+        timer.loop(Phaser.Timer.SECOND, updateTime, this);
+        timer.start();
+
 
     },
 
     update: function () {
 
-        //set collision
-        if(game.physics.arcade.collide(player, ground)){
-            //console.log("collision");
-            //gameOver();
-        }        
+        // //set collision
+        // if(game.physics.arcade.collide(player, ground)){
+        //     //console.log("collision");
+        //     //gameOver();
+        // }        
 
         //input
         if(input.left.isDown){
-            player.body.angularAcceleration = -360;
-            
+            player.body.angularVelocity -= 0.1;
+
         }
         else if(input.right.isDown){
-            player.body.angularAcceleration = 360;            
+            player.body.angularVelocity += 0.1;            
         }
         else{            
-            if(player.body.angularVelocity < -50)
-                player.body.angularAcceleration = 360;
-            else if(player.body.angularVelocity > 50)
-                player.body.angularAcceleration = -360;
+            if(player.body.angularVelocity < -5)
+                player.body.angularVelocity += 0.1;
+            else if(player.body.angularVelocity > 5)
+                player.body.angularVelocity -= 0.1;
             else{
                 player.body.angularVelocity = 0;
-                player.body.angularAcceleration = 0;
                 }
         }
         if(player.body.angularVelocity < player.minAngVelo)
@@ -121,47 +142,26 @@ var Game = {
         if(player.body.angularVelocity > player.maxAngVelo)
             player.body.angularVelocity = player.maxAngVelo;
 
-
-        if(input.up.isDown){
-            player.fuel -= 1;
-            player.body.acceleration.y = Math.cos(player.body.rotation/R) * (-15);
-            player.body.acceleration.x = Math.sin(player.body.rotation/R) * (15);
-
-        }
-        // else if(input.down.isDown){
-        //     //player.acce = -30;
-        //     //player.body.velocity.x = Math.sin(player.body.rotation/R) * player.acce;
-        //     player.body.acceleration.y = Math.cos(player.body.rotation/R) * 15;
-        //     player.body.acceleration.x = Math.sin(player.body.rotation/R) * (-15);
-        // }
-        else{
-            player.body.acceleration.x = 0;
-            player.body.acceleration.y = 0;
-        }
-
-        //detect borders
-        player.body.collideWorldBounds = true;
-        // if(player.x < 0 || player.x > 1600){
-        //     player.x -= player.body.velocity.x;
-        // }
-        // if(player.y < 0 || player.y > 900){
-        //     player.y -= player.body.velocity.y;
-        // }
+        player.body.velocity.y += Math.cos(player.body.rotation) * (-0.2) * player.engineLevel;
+        player.body.velocity.x += Math.sin(player.body.rotation) * (0.2) * player.engineLevel;
+        player.fuel -= 0.5 * player.engineLevel;
 
         //update text
         scoreText.text = 'Score: ' + score;
         timeText.text = 'Time: ' + time;
         fuelText.text = 'Fuel: ' + player.fuel;
         altiText.text = 'Altitude: ' + parseInt(900 - player.y);
-        speedXText.text = 'Horizontal Speed: ' + parseInt(player.body.velocity.x);
-        speedYText.text = 'Vertical Speed: ' + parseInt(player.body.velocity.y);
+        speedXText.reset(player.x + 30, player.y - 30);
+        speedXText.text = 'SpeedX: ' + parseInt(player.body.velocity.x);
+        speedYText.reset(player.x + 30, player.y - 15);
+        speedYText.text = 'SpeedY: ' + parseInt(player.body.velocity.y);
 
         //update particle
-        emitter.x = player.x - Math.sin(player.body.rotation/R) * 24;
-        emitter.y = player.y + Math.cos(player.body.rotation/R) * 24;
+        emitter.x = player.x - Math.sin(player.body.rotation) * 24;
+        emitter.y = player.y + Math.cos(player.body.rotation) * 24;
         emitter.setRotation(0, 360);
-        emitter.setXSpeed(0, -player.body.acceleration.x*20);
-        emitter.setYSpeed(0, -player.body.acceleration.y*20);
+        emitter.setXSpeed(0, -player.body.velocity.x * 5);
+        emitter.setYSpeed(0, -player.body.velocity.y * 5);
 
         //camera
         game.camera.follow(player);
@@ -174,4 +174,15 @@ var Game = {
 
 function gameOver(){
     console.log('Game Over');
+}
+
+function updateTime() {
+
+    time += 1;
+    timeText.text = 'Time: ' + time;
+
+}
+
+function rebirth(){
+
 }
