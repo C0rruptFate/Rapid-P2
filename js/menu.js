9bdd5c8166e591487713a8027c1685214e30586f
Zhitao <31802he@163.com>
2017-10-03 09:17:20 -0600

    1

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,11 @@
 var Menu = {
 
 	preload: function () {
+		
 		game.load.image('menu', 'images/Art/GUI/menu.png');
+
+		//load video
+        game.load.video('video', 'images/Art/GUI/MenuVideo.mov')
 	},
 
 	create: function () {
